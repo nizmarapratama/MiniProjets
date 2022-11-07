63aa4ab1329ea4c57d1aeceedf814d24977a8034
nizmarapratama <nizmarapratama.@gmail.com>
2022-11-08 02:09:00 +0700

    Kasir App

--- KasirApp/kasir-app/src/components/TotalBayar.js
+++ KasirApp/kasir-app/src/components/TotalBayar.js
@@ -5,6 +5,7 @@ import React, { Component } from "react";
 import { Row, Col, Button } from "react-bootstrap";
 import { numberWithCommas } from "../utils/utils";
 import { API_URL } from '../utils/constants'
+import { Link } from "react-router-dom";
 
 export default class TotalBayar extends Component {
   submitTotalBayar = (totalBayar) => {
@@ -27,26 +28,22 @@ export default class TotalBayar extends Component {
       <>
       {/* Web */}
       <div className="fixed-bottom d-none d-md-block">
-        <Row>
-          <Col md={{ span: 3, offset: 9 }} className="px-4">
-            <h4>
-              Total Harga :{" "}
-              <strong className="float-right mr-2">
-                Rp. {numberWithCommas(totalBayar)}
-              </strong>
-            </h4>
-            <Button
-              variant="primary"
-              block
-              className="mb-2 mt-4 mr-2"
-              size="lg"
-              onClick={() => this.submitTotalBayar(totalBayar)}
-            >
-              <FontAwesomeIcon icon={faShoppingCart} /> <strong>BAYAR</strong>
-            </Button>
-          </Col>
-        </Row>
-      </div>
+          <Row>
+            <Col md={{ span: 3, offset: 9 }} className="px-4">
+              <h4>
+                <div className="float-right">
+                  Total Harga : <strong className="ms-2">Rp. {numberWithCommas(totalBayar)}</strong>
+                </div>
+              </h4>
+              <div className="d-grid gap-4">
+              <Button variant="primary" size="lg" className="mb-2 mt-2 me-3" onClick={() => this.submitTotalBayar(totalBayar)} as={Link} to="/">
+                  <FontAwesomeIcon icon={faShoppingCart} />
+                  <strong>BAYAR</strong>
+                </Button>{" "}
+              </div>
+            </Col>
+          </Row>
+        </div>
 
       {/* Mobile  */}
       <div className="d-sm-block d-md-none">
